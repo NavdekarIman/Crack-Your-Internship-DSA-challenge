b107bf701b91fc8af1f5ea29494bef3ecb5a1100
NavdekarIman <imannavdekar08@gmail.com>
2024-07-17 17:17:02 +0530

    Added 75 Sort Colors

---
+++ Array/287_Find_the_Duplicate_Number.java
@@ -0,0 +1,23 @@
+
+// class Solution {
+//     public int findDuplicate(int[] nums) {
+//         Arrays.sort(nums);
+//         int ans=0;
+//         for(int i=0;i<nums.length-1;i++)
+//             if(nums[i]==nums[i+1])
+//                 ans=nums[i];
+//         return ans;
+//     }
+// }
+
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int n=nums.length;
+        boolean [] arr = new boolean[n];
+        for(int i = 0;i< n; i++){
+            if(arr[nums[i]]) return nums[i];
+            arr[nums[i]] = true;
+        }
+        return - 1;
+    }
+}
